d0648f4f9127ee8de270a38d66d7c24eff6ef0a9
rmadhavan25 <rmadhavan718@gmail.com>
2022-12-16 14:54:42 +0530

    fully responsive portfolio template

--- src/components/workCard/WorkCard.js
+++ src/components/workCard/WorkCard.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import './workCard.css';
-
-class WorkCard extends React.Component{
-    render(){
-        return(
-            <Container className="workCard-container mt-5">
-                <p>Title</p>
+// {require("E:/portfolio/src/assets/tailor-made-2.png")}
+class WorkCard extends React.Component {
+    render() {
+        return (
+            <Container className="workCard-container mt-4">
+                <Card>
+                    <Card.Img variant="top" src={require("E:/portfolio/src/assets/tailor-made-2.png")} />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
             </Container>
+
         );
     }
 }
